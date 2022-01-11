dae2da4dac1b5a24e8ed4f83bb9b983cb37236a7
elnuralimirzayev <aliveit.elnur@gmail.com>
2022-01-11 23:52:07 +0100

    Stage commit

--- src/server/server.c
+++ src/server/server.c
@@ -2,7 +2,51 @@
 // Created by al1ve1t on 11.12.21.
 //
 
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define DEFAULT_PORT 1080
+
+#ifndef PORT
+#define PORT DEFAULT_PORT
+#endif
+
 void RunServer() {
+    struct sockaddr_in serveraddr;
+    int sockfd;
+    // Create and verify socket
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+        //failed
+        exit(1);
+    } else {
+        //created
+    }
+    bzero(&serveraddr, sizeof(serveraddr));
+
+    // Assign IP and PORT
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serveraddr.sin_port = htons(PORT);
+
+    // Bind
+    if (bind(sockfd, (struct sockaddr*)&serveraddr, sizeof(serveraddr)) != 0) {
+        // failed
+    } else {
+        // created
+    }
+
+    // Listen
+    if (listen(sockfd, 5) != 0) {
+        // failed
+    } else {
+        // success
+    }
+
+    // Accept
 
 }
 
