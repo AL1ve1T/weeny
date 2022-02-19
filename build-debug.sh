eda7e27d5568b81ea27def8e25462ccf2ea30b2d
Elnur Alimirzayev <aliveit.elnur@gmail.com>
2022-02-19 19:54:17 +0000

    Unused files removed

--- build-debug.sh
+++
@@ -1,5 +0,0 @@
-# Configure the build
-cmake -S . -B build/Debug -D CMAKE_BUILD_TYPE=Debug
-
-# Actually build the binaries
-cmake --build build/Debug
--- build-release.sh
+++
@@ -1,5 +0,0 @@
-# Configure the build
-cmake -S . -B build/Release -D CMAKE_BUILD_TYPE=Release
-
-# Actually build the binaries
-cmake --build build/Release
