8cdcb1199e46a297964b0d7c2a176e5b1a1c39dd
Elnur Alimirzayev <aliveit.elnur@gmail.com>
2022-02-19 20:02:15 +0000

    Added configuration for zlog in build-deps.sh

--- build-deps.sh
+++ build-deps.sh
@@ -9,6 +9,12 @@ make PREFIX=/usr/local install
 ldconfig
 echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib" >> ~/.bashrc
 
+# configure zlog
+echo "[formats]
+simple = "%m%n"
+[rules]
+default.DEBUG    >stdout; simple" >> /etc/zlog
+
 # return to root folder
 cd ../..
 
